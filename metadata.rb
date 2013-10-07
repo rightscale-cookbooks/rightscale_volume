e39afc66233a86215274d3bcafce2a6d6a5eb553
Nitin Mohan <nitin@rightscale.com>
2013-10-07 13:21:55 -0700

    acu111809 - [rightscale_volume] Remove this test recipe since we now have a dedicated wrapper cookbook for testing

--- metadata.rb
+++ metadata.rb
@@ -7,4 +7,3 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.1.0'
 
 recipe "rightscale_volume::default", "Default recipe for installing required packages/gems."
-recipe "rightscale_volume::test", "Recipe to run functional tests."
