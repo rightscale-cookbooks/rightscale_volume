a9549887397e5fb0e36af3683771220007bdaaef
Kannan Manickam <kannan@rightscale.com>
2014-04-01 12:01:56 -0700

    Do not include delayed_evaluator.

--- test/cookbooks/test-rightscale_volume/recipes/test.rb
+++ test/cookbooks/test-rightscale_volume/recipes/test.rb
@@ -23,10 +23,6 @@ end
 
 include_recipe 'rightscale_volume::default'
 
-# Include cookbook-delayed_evaluator for delaying evaluation of node attributes
-# to converge phase instead of compile phase
-include_recipe 'delayed_evaluator'
-
 # Set minimum volume size to 100GB for Rackspace Open Clouds (cloud-specific feature)
 volume_size = node[:cloud][:provider] == 'rackspace-ng' ? 100 : 1
 
