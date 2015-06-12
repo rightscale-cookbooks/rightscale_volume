c058f8232d86d9c003f947809dac11b70179db8d
Lopaka Delp <lopaka@rightscale.com>
2015-06-12 15:39:51 -0700

    ST-383 Minor changes to log output.

--- libraries/provider_rightscale_volume.rb
+++ libraries/provider_rightscale_volume.rb
@@ -132,7 +132,7 @@ class Chef
         # by backups.  Unfortunatly, CloudStack still allows you to delete the volume.
         if node['cloud']['provider'] == 'cloudstack' && node['virtualization']['system'] == 'kvm'
           Chef::Log.info "Volume '#{@current_resource.nickname}' was not deleted based on current" +
-            "cloud and hypervisor - #{node['cloud']['provider']} #{node['virtualization']['system']}"
+            " cloud and hypervisor - '#{node['cloud']['provider']}' '#{node['virtualization']['system']}'"
           delete_device_hash
           return
         end
